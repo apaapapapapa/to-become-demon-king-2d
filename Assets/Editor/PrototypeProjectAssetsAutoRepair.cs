bca19c16eb8c19d95bfa9af5de1696ee66a85188
apapaapapapapa <83394495+apaapapapapa@users.noreply.github.com>
2026-07-19 22:49:44 +0900

    refactor: ProjectAssets自動修復へApplicationとDodge設定を追加

--- Assets/Editor/PrototypeProjectAssetsAutoRepair.cs
+++ Assets/Editor/PrototypeProjectAssetsAutoRepair.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using DemonKing.Field.Prototype;
+using DemonKing.Field.Prototype.Configuration;
 using DemonKing.Gameplay.Characters.Configuration;
 using DemonKing.Gameplay.Combat.Configuration;
 using UnityEditor;
@@ -16,9 +17,11 @@ namespace DemonKing.EditorTools
     {
         private const string FontInstallAttemptSessionKey = "DemonKing.FontInstallAttempted";
         private const string ProjectAssetsPath = "Assets/Resources/Settings/PrototypeProjectAssets.asset";
+        private const string ApplicationSettingsPath = "Assets/Resources/Settings/PrototypeApplicationSettings.asset";
         private const string PlayerPrefabPath = "Assets/Resources/Prefabs/Characters/PrototypeSlime.prefab";
         private const string PlayerCharacterStatsPath = "Assets/Resources/Settings/Gameplay/PlayerCharacterStats.asset";
         private const string PlayerMeleeAttackPath = "Assets/Resources/Settings/Gameplay/PlayerMeleeAttack.asset";
+        private const string PlayerDodgePath = "Assets/Resources/Settings/Gameplay/PlayerDodge.asset";
         private const string CottagePrefabPath = "Assets/Resources/Prefabs/World/PrototypeCottage.prefab";
         private const string TreePrefabPath = "Assets/Resources/Prefabs/World/PrototypeTree.prefab";
         private const string LamppostPrefabPath = "Assets/Resources/Prefabs/World/PrototypeLamppost.prefab";
@@ -73,9 +76,11 @@ namespace DemonKing.EditorTools
             SerializedObject serializedObject = new(projectAssets);
             bool changed = false;
 
+            changed |= AssignIfDifferent(serializedObject, "applicationSettings", Load<PrototypeApplicationSettings>(ApplicationSettingsPath));
             changed |= AssignIfDifferent(serializedObject, "playerPrefab", Load<GameObject>(PlayerPrefabPath));
             changed |= AssignIfDifferent(serializedObject, "playerCharacterStats", Load<CharacterStatsDefinition>(PlayerCharacterStatsPath));
             changed |= AssignIfDifferent(serializedObject, "playerMeleeAttack", Load<MeleeAttackDefinition>(PlayerMeleeAttackPath));
+            changed |= AssignIfDifferent(serializedObject, "playerDodge", Load<DodgeDefinition>(PlayerDodgePath));
             changed |= AssignIfDifferent(serializedObject, "uiFont", Load<Font>(JapaneseUiFontInstaller.FontAssetPath, logIfMissing: forceLog));
             changed |= AssignIfDifferent(serializedObject, "cottagePrefab", Load<GameObject>(CottagePrefabPath));
             changed |= AssignIfDifferent(serializedObject, "treePrefab", Load<GameObject>(TreePrefabPath));
