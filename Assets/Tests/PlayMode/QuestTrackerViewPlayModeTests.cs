13c07dd513f4d23a382a98ed21f781d7c931717c
apapaapapapapa <83394495+apaapapapapa@users.noreply.github.com>
2026-07-21 06:26:54 +0900

    test: add quest tracker ui flow coverage

---
+++ Assets/Tests/PlayMode/QuestTrackerViewPlayModeTests.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using DemonKing.Domain.Quests;
+using DemonKing.Field.Prototype;
+using DemonKing.Gameplay.Events;
+using DemonKing.Gameplay.Quests;
+using DemonKing.Gameplay.Quests.Configuration;
+using DemonKing.Presentation.UI;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+
+namespace DemonKing.Tests.PlayMode
+{
+    public sealed class QuestTrackerViewPlayModeTests
+    {
+        [UnityTest]
+        public IEnumerator QuestTrackerView_受注進捗完了を表示する()
+        {
+            QuestDefinition quest = QuestDefinition.CreateRuntime(
+                "quest.test.ui",
+                "表示テストQuest",
+                QuestObjectiveDefinition.CreateRuntime(
+                    "objective.test.ui",
+                    GameplayEventIds.EnemyDefeated,
+                    "character.training_dummy",
+                    2,
+                    "訓練用スライムを倒す"));
+            var service = new QuestProgressionService(new[] { quest });
+
+            GameObject uiRoot = new("Quest UI Test", typeof(RectTransform));
+            uiRoot.AddComponent<Canvas>();
+            QuestTrackerView view = uiRoot.AddComponent<QuestTrackerView>();
+            Font font = Resources.Load<PrototypeProjectAssets>("Settings/PrototypeProjectAssets").UiFont;
+            view.Initialize(font, service);
+
+            Assert.That(view.IsVisible, Is.False);
+            Assert.That(view.IsNotificationVisible, Is.False);
+
+            service.AcceptQuest(quest.QuestId);
+
+            Assert.That(view.IsVisible, Is.True);
+            Assert.That(view.DisplayedStatusText, Is.EqualTo("受注中"));
+            Assert.That(view.DisplayedQuestTitle, Is.EqualTo("表示テストQuest"));
+            Assert.That(view.DisplayedObjectiveText, Does.Contain("訓練用スライムを倒す"));
+            Assert.That(view.DisplayedObjectiveText, Does.Contain("0/2"));
+            Assert.That(view.IsNotificationVisible, Is.True);
+            Assert.That(view.DisplayedNotificationText, Does.Contain("クエスト受注"));
+
+            service.Handle(new GameplayEvent(
+                GameplayEventIds.EnemyDefeated,
+                "character.training_dummy"));
+
+            Assert.That(view.DisplayedStatusText, Is.EqualTo("受注中"));
+            Assert.That(view.DisplayedObjectiveText, Does.Contain("1/2"));
+            Assert.That(view.DisplayedNotificationText, Does.Contain("クエスト進捗"));
+
+            service.Handle(new GameplayEvent(
+                GameplayEventIds.EnemyDefeated,
+                "character.training_dummy"));
+
+            Assert.That(view.DisplayedStatusText, Is.EqualTo("完了"));
+            Assert.That(view.DisplayedObjectiveText, Does.Contain("2/2"));
+            Assert.That(view.DisplayedObjectiveText, Does.Contain("✓"));
+            Assert.That(view.DisplayedNotificationText, Does.Contain("クエスト完了"));
+            Assert.That(
+                uiRoot.GetComponentsInChildren<Text>(includeInactive: true).Length,
+                Is.GreaterThanOrEqualTo(4));
+
+            Object.Destroy(uiRoot);
+            Object.Destroy(quest);
+            yield return null;
+        }
+    }
+}
