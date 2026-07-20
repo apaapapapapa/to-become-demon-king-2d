e8432cf09f5e4cf906bc60d62e0aaf63dda81f99
apapaapapapapa <83394495+apaapapapapa@users.noreply.github.com>
2026-07-21 08:24:42 +0900

    refactor: consolidate prototype training scenario references

--- Assets/Scripts/Field/Prototype/PrototypeProjectAssets.cs
+++ Assets/Scripts/Field/Prototype/PrototypeProjectAssets.cs
@@ -1,14 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using DemonKing.Field.Prototype.Configuration;
-using DemonKing.Gameplay.AI.Configuration;
 using DemonKing.Gameplay.Characters.Configuration;
 using DemonKing.Gameplay.Content;
-using DemonKing.Gameplay.Dialogue.Configuration;
-using DemonKing.Gameplay.Progression.Configuration;
 using DemonKing.Gameplay.Quests.Configuration;
-using DemonKing.Gameplay.Rewards.Configuration;
 using UnityEngine;
 
 namespace DemonKing.Field.Prototype
@@ -25,18 +19,9 @@ namespace DemonKing.Field.Prototype
 
         [Header("Characters")]
         [SerializeField] private CharacterDefinition playerCharacter;
-        [SerializeField] private EnemyAiDefinition trainingSlimeAi;
 
-        [Header("Content")]
-        [SerializeField] private DialogueDefinition apprenticeMageDialogue;
-        [SerializeField] private DialogueDefinition apprenticeMageActiveDialogue;
-        [SerializeField] private DialogueDefinition apprenticeMageTurnInDialogue;
-        [SerializeField] private DialogueDefinition apprenticeMageCompletedDialogue;
-        [SerializeField] private QuestDefinition[] questDefinitions = Array.Empty<QuestDefinition>();
-
-        [Header("Rewards")]
-        [SerializeField] private RewardDefinition trainingDummyReward;
-        [SerializeField] private ProgressionGrantDefinition fireMagicTrainingGrant;
+        [Header("Scenarios")]
+        [SerializeField] private TrainingScenarioDefinition trainingScenario;
 
         [Header("UI")]
         [SerializeField] private Font uiFont;
@@ -57,15 +42,11 @@ namespace DemonKing.Field.Prototype
 
         public PrototypeApplicationSettings ApplicationSettings => applicationSettings;
         public CharacterDefinition PlayerCharacter => playerCharacter;
-        public EnemyAiDefinition TrainingSlimeAi => trainingSlimeAi;
-        public DialogueDefinition ApprenticeMageDialogue => apprenticeMageDialogue;
-        public DialogueDefinition ApprenticeMageActiveDialogue => apprenticeMageActiveDialogue;
-        public DialogueDefinition ApprenticeMageTurnInDialogue => apprenticeMageTurnInDialogue;
-        public DialogueDefinition ApprenticeMageCompletedDialogue => apprenticeMageCompletedDialogue;
-        public QuestDefinition[] QuestDefinitions => questDefinitions ?? Array.Empty<QuestDefinition>();
-        public QuestDefinition TrainingQuestDefinition => QuestDefinitions.FirstOrDefault();
-        public RewardDefinition TrainingDummyReward => trainingDummyReward;
-        public ProgressionGrantDefinition FireMagicTrainingGrant => fireMagicTrainingGrant;
+        public TrainingScenarioDefinition TrainingScenario => trainingScenario;
+        public QuestDefinition TrainingQuestDefinition => trainingScenario?.QuestDefinition;
+        public QuestDefinition[] QuestDefinitions => trainingScenario?.QuestDefinition == null
+            ? Array.Empty<QuestDefinition>()
+            : new[] { trainingScenario.QuestDefinition };
         public Font UiFont => uiFont;
         public bool HasUiFont => uiFont != null;
         public GameObject CottagePrefab => cottagePrefab;
@@ -85,33 +66,7 @@ namespace DemonKing.Field.Prototype
                     "GameContentCatalogを作成するためのPlayerCharacterが設定されていません。");
             }
 
-            var definitions = new List<IGameContentDefinition> { playerCharacter };
-
-            foreach (var abilityDefinition in playerCharacter.AbilityDefinitions)
-            {
-                definitions.Add(abilityDefinition);
-            }
-
-            foreach (ArtDefinition artDefinition in playerCharacter.ArtDefinitions)
-            {
-                definitions.Add(artDefinition);
-                foreach (ArtAbilityUnlockEntry unlockEntry in artDefinition.AbilityUnlocks)
-                {
-                    definitions.Add(unlockEntry.AbilityDefinition);
-                }
-            }
-
-            foreach (SkillDefinition skillDefinition in playerCharacter.SkillDefinitions)
-            {
-                definitions.Add(skillDefinition);
-            }
-
-            foreach (EvolutionDefinition evolutionDefinition in playerCharacter.EvolutionDefinitions)
-            {
-                definitions.Add(evolutionDefinition);
-            }
-
-            return new GameContentCatalog(definitions);
+            return new GameContentCatalog(new IGameContentDefinition[] { playerCharacter });
         }
 
         /// <summary>
@@ -122,24 +77,8 @@ namespace DemonKing.Field.Prototype
             applicationSettings != null &&
             playerCharacter != null &&
             playerCharacter.IsConfigured &&
-            trainingSlimeAi != null &&
-            trainingSlimeAi.IsConfigured &&
-            apprenticeMageDialogue != null &&
-            apprenticeMageDialogue.IsConfigured &&
-            apprenticeMageActiveDialogue != null &&
-            apprenticeMageActiveDialogue.IsConfigured &&
-            apprenticeMageTurnInDialogue != null &&
-            apprenticeMageTurnInDialogue.IsConfigured &&
-            apprenticeMageCompletedDialogue != null &&
-            apprenticeMageCompletedDialogue.IsConfigured &&
-            questDefinitions != null &&
-            questDefinitions.Length > 0 &&
-            questDefinitions.All(definition => definition != null && definition.IsConfigured) &&
-            questDefinitions.Select(definition => definition.QuestId).Distinct(StringComparer.Ordinal).Count() == questDefinitions.Length &&
-            trainingDummyReward != null &&
-            trainingDummyReward.IsConfigured &&
-            fireMagicTrainingGrant != null &&
-            fireMagicTrainingGrant.IsConfigured &&
+            trainingScenario != null &&
+            trainingScenario.IsConfigured &&
             cottagePrefab != null &&
             treePrefab != null &&
             lamppostPrefab != null &&
