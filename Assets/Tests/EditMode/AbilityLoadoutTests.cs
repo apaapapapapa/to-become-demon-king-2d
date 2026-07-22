d0f56da5f3d7238972151d10d254f084add1a3be
apapaapapapapa <83394495+apaapapapapa@users.noreply.github.com>
2026-07-22 09:12:28 +0900

    test: cover loadout policy eligibility and uniqueness

--- Assets/Tests/EditMode/AbilityLoadoutTests.cs
+++ Assets/Tests/EditMode/AbilityLoadoutTests.cs
@@ -1,4 +1,6 @@
 using DemonKing.Core.Input;
+using DemonKing.Domain.Progression;
+using DemonKing.Field.Prototype;
 using DemonKing.Gameplay.Abilities;
 using NUnit.Framework;
 using UnityEngine;
@@ -29,6 +31,58 @@ namespace DemonKing.Tests.EditMode
             bool changed = loadout.Assign(AbilitySlot.Action1, " ability.test.same ");
 
             Assert.That(changed, Is.False);
+        }
+
+        [Test]
+        public void Assign_SameAbilityToDifferentEditableSlot_MovesAssignment()
+        {
+            var loadout = new AbilityLoadout();
+            loadout.Assign(AbilitySlot.Action1, "ability.test.unique");
+
+            bool changed = loadout.Assign(AbilitySlot.Action3, "ability.test.unique");
+
+            Assert.That(changed, Is.True);
+            Assert.That(loadout.TryResolve(AbilitySlot.Action1, out _), Is.False);
+            Assert.That(loadout.TryResolve(AbilitySlot.Action3, out string abilityId), Is.True);
+            Assert.That(abilityId, Is.EqualTo("ability.test.unique"));
+        }
+
+        [Test]
+        public void AbilityLoadoutPolicy_PrimaryIsReservedAndActionSlotsAreEditable()
+        {
+            Assert.That(AbilityLoadoutPolicy.IsEditableSlot(AbilitySlot.Primary), Is.False);
+            Assert.That(
+                AbilityLoadoutPolicy.EditableSlots,
+                Is.EqualTo(new[]
+                {
+                    AbilitySlot.Action1,
+                    AbilitySlot.Action2,
+                    AbilitySlot.Action3,
+                    AbilitySlot.Action4
+                }));
+        }
+
+        [Test]
+        public void AbilityLoadoutEligibility_LearnedArtExposesAssignableAbility()
+        {
+            PrototypeProjectAssets projectAssets =
+                Resources.Load<PrototypeProjectAssets>("Settings/PrototypeProjectAssets");
+            var definition = projectAssets.PlayerCharacter;
+            CharacterProgressionState state =
+                CharacterProgressionState.CreateInitial(definition.CharacterId);
+
+            Assert.That(definition.ArtDefinitions.Count, Is.GreaterThan(0));
+            Assert.That(state.TryLearnArt(definition.ArtDefinitions[0].ArtId, out _), Is.True);
+
+            var eligible = AbilityLoadoutEligibility.GetAssignableAbilities(definition, state);
+
+            Assert.That(eligible.Count, Is.GreaterThan(0));
+            Assert.That(
+                AbilityLoadoutEligibility.CanAssign(
+                    definition,
+                    state,
+                    eligible[0].Ability.AbilityId),
+                Is.True);
         }
 
         [Test]
